94a7cbb23667fddbfc82d94cf7471ce89929bb6c
AnasBenba <anasbenba123@gmail.com>
2023-04-26 09:41:39 -0700

    fix code

--- b_env.c
+++ b_env.c
@@ -8,27 +8,6 @@
 void builtin(void)
 {
 	char **ev;
-	char *tmp;
-	int count = 0;
-	int i, j;
-
-	for (ev = environ; *ev != NULL; ev++)
-	{
-		count++;
-	}
-
-	for (i = 0; i < count -1; i++)
-	{
-		for (j = i + 1; j < count; j++)
-		{
-			if (_strcmp(environ[i], environ[j]) > 0)
-			{
-				tmp = environ[i];
-				environ[i] = environ[j];
-				environ[j] = tmp;
-			}
-		}
-	}
 
 	for (ev = environ; *ev != NULL; ev++)
 	{
